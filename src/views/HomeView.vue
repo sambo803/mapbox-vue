af5ae9c25e52c3006b34ec6f355a8b56d7bdc570
samuel <sbacon803@gmail.com>
2022-09-10 14:23:26 -0600

    add mapbox vue practice file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
+<script>
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!!!",
+    };
+  },
+  mounted: function () {
+    this.makeMap();
+  },
+  methods: {
+    makeMap: function () {
+      console.log("making map...");
+      mapboxgl.accessToken =
+        "";
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+        projection: "globe", // display the map as a 3D globe
+      });
+      map.on("style.load", () => {
+        map.setFog({}); // Set the default atmosphere style
+      });
+    },
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div id="map" style="width: 400px; height: 300px"></div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
